4b74b397e15e87b59f95afd6a0d0611666a3fae9
MahimaRaol <158015664+MahimaRaol@users.noreply.github.com>
2025-01-22 06:45:21 +0000

    ok

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,85 @@
+let score = 33
+let age = "27"
+
+// console.log(typeof score);
+// console.log(typeof (score));
+// console.log(typeof (age));
+
+let valueInNumber = Number(score)
+// console.log(valueInNumber);
+// console.log(typeof valueInNumber);
+
+let score1 = "11abc"
+// console.log(score1)
+// console.log(typeof score1)
+
+let valueInNumber1 = Number(score1)
+// console.log(valueInNumber1);
+// console.log(typeof valueInNumber1);
+
+let score12 = null
+// console.log(score12)
+// console.log(typeof score12)
+// console.log(typeof (score12))
+
+
+let valueInNumber12 = Number(score12)
+// console.log(typeof valueInNumber12);
+// console.log(valueInNumber12);
+
+let score123 = undefined
+// console.log(score123)
+// console.log(typeof score123)
+// console.log(typeof (score123))
+
+
+let valueInNumber123 = Number(score123)
+// console.log(typeof valueInNumber123);
+// console.log(valueInNumber123);
+
+let score1234 = true
+// console.log(score1234)
+// console.log(typeof score1234)
+// console.log(typeof (score1234))
+
+
+let valueInNumber1234 = Number(score1234)
+// console.log(typeof valueInNumber1234);
+// console.log(valueInNumber1234);
+
+let score12345 = "Mahima"
+// console.log(score12345)
+// console.log(typeof score12345)
+// console.log(typeof (score12345))
+
+
+let valueInNumber12345 = Number(score12345)
+// console.log(typeof valueInNumber12345);
+// console.log(valueInNumber12345);
+
+// "33" => 33
+// "33abc" => NaN
+//  true => 1; false => 0
+
+let isLoggedIn = 1
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+//console.log(booleanIsLoggedIn)
+
+let isLoggedIn1 = "mahima"
+let booleanIsLoggedIn1 = Boolean(isLoggedIn1)
+//console.log(booleanIsLoggedIn1)
+
+let isLoggedIn12 = ""
+let booleanIsLoggedIn12 = Boolean(isLoggedIn12)
+//console.log(booleanIsLoggedIn12)
+
+// 1 => true; 0 => false
+// "" => false
+// "hitesh" => true
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
